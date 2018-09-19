f18284a3d0670af3713915624ce9a69d1a404145
kithminisg <kithminishanika@gmail.com>
2018-09-19 23:51:17 +0530

    POS tagging

--- com-logic/Natbool.py
+++ com-logic/Natbool.py
@@ -1,29 +1,40 @@
-# ======================Tokenizing=========================
-# from nltk.tokenize \
-#     import sent_tokenize,word_tokenize
-#
-# example_text = "Hello there, how are you doing today? The weather is great and Pythom is awesome.The sky is blue. Do not eat card board"
-#
-# print(sent_tokenize(example_text))
-# print(word_tokenize(example_text))
 
-#===============Stop Words=================================
+import nltk
 
 from nltk.corpus import stopwords
-from nltk.tokenize import word_tokenize
+from nltk.tokenize import word_tokenize,sent_tokenize
 
-example_sentence = "If the door is opened then the blub is switched-off"
+
+
+#=================Text data aquisition===============================
+with open('data.txt', 'r') as myfile:
+    example_sentence=myfile.read().replace('\n', '')
+
+#===============Stop Words in English ===============================
 stop_words = set(stopwords.words("english"))
 
-words = word_tokenize(example_sentence)
-filtered_sentence = []
-for w in words:
-    if w not in stop_words:
-        filtered_sentence.append(w)
+#==================Tokenize the sentences============================
 
-print(filtered_sentence)
+sents = sent_tokenize(example_sentence)
+print(sents)
+#========================NE Recognition==================================
 
+def ne_addition(sents):
+    try:
+        for i in sents:
+            words = word_tokenize(i)
+            filtered_sentence = []
+            for w in words:
+                if w not in stop_words:
+                    filtered_sentence.append(w)
+            tagged = nltk.pos_tag(filtered_sentence)
+            print(tagged)
+            namedEnt = nltk.ne_chunk(tagged)
+            namedEnt.draw()
+    except Exception as e:
+        print(str(e))
 
+ne_addition(sents)
 
 #====================Stemming=================================
 '''
@@ -35,9 +46,18 @@ print(filtered_sentence)
 
     example_words = ["python","pythoning","pythoner","pythoned","pythonly"]
 
+       # chunk_gram = r"""Chunk:{<NN>$}"""
+            # chunk_parser = nltk.RegexpParser(chunk_gram)
+            # chunked_data  = chunk_parser.parse(tagged)
+            # print(chunked_data)
+            # chunked_data.draw();
+
+
+
     for w in example_words:
         print(ps.stem(w))
 '''
 #=============================================================
+
 
 

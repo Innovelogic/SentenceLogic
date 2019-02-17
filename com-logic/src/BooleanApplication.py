42f31a484adf84796e495a60e50e69d2bf14ac47
kithminisg <kithminishanika@gmail.com>
2019-02-17 11:27:05 +0530

    EntitiyRef and Sentence Filter

--- com-logic/src/BooleanApplication.py
+++ com-logic/src/BooleanApplication.py
@@ -1,12 +1,48 @@
 import EntityExtractor,InputOutputIdentifier
 from EntityExtractor import *
 from InputOutputIdentifier import *
+from SentenceFilter import *
+from EntityReferencer import *
+
+with open('../data/data2.txt', 'r') as myfile:
+    example_sentence = myfile.read().replace('\n', '')  # example text is read from data2.txt
 
 
-def main():
+def entity_extraction_accuracy_measure():
+    return 0
 
-    with open('../data/data2.txt', 'r') as myfile:
-        example_sentence = myfile.read().replace('\n', '')  # example text is read from data2.txt
+
+def input_output_identification_accuracy_measure():
+    return 0
+
+
+def conversion_method(sentences,input_output_identifier):
+
+    print("Example :", example_sentence)
+    print("Inputs :", input_output_identifier.input_set)
+    print("Outputs :", input_output_identifier.output_set)
+
+    input_output_dic = {}
+    x = ord('A')
+    for input in input_output_identifier.input_set:
+        input_output_dic['I' + chr(x)] = input
+        x = x + 1
+
+    x = ord('A')
+    for output in input_output_identifier.output_set:
+        input_output_dic['O' + chr(x)] = output
+        x = x + 1
+    print(input_output_dic)
+    conversion = sentences
+    for word in input_output_dic:
+        conversion = conversion.replace(input_output_dic[word], input_output_dic[word]+" [" + word+"]")
+
+    print(conversion)
+
+
+def method_one():
+    print("===========================Regex based normal Extraction Approach============================================")
+
     entity_extractor = EntityExtractor(example_sentence)
     entity_extractor.extraction_with_regex()
     print(entity_extractor.entitySet)
@@ -14,8 +50,67 @@ def main():
     input_output_identifier = InputOutputIdentifier(entity_extractor.entitySet,example_sentence)
     input_output_identifier.neighbourhood_based_identification()
 
-    print("Example :",example_sentence)
-    print("Inputs :",input_output_identifier.input_set)
-    print("Outputs :",input_output_identifier.output_set)
+    conversion_method(example_sentence, input_output_identifier)
 
-main()
+    print("=============================================================================================================\n")
+
+
+def method_two():
+
+
+    print("========================Regex based Verb analysed Extraction Approach========================================")
+
+    # Create Entity Extractor Object
+    entity_extractor = EntityExtractor(sample_sentences)
+    entity_extractor.extraction_verb_based()
+    print(entity_extractor.entitySet)
+
+    print(sample_sentences)
+    input_output_identifier = InputOutputIdentifier(entity_extractor.entitySet,sample_sentences)
+    input_output_identifier.neighbourhood_based_identification()
+
+
+
+    conversion_method(sample_sentences,input_output_identifier)
+    print("=============================================================================================================\n")
+
+
+def method_three():
+
+    print("====================================NER based Extraction Approach============================================")
+    entity_extractor = EntityExtractor(example_sentence)
+    entity_extractor.extraction_with_ner()
+    print(entity_extractor.entitySet)
+
+    input_output_identifier = InputOutputIdentifier(entity_extractor.entitySet,example_sentence)
+    input_output_identifier.neighbourhood_based_identification()
+
+    conversion_method(example_sentence,input_output_identifier)
+    print("=============================================================================================================")
+
+
+# Create sentence filter object
+sentence_filter = SentenceFilter(example_sentence)
+
+# Filtering sentences for description sentences and to logic sentences
+sentence_filter.filter_sentences()
+
+if sentence_filter.description_sentences:
+    # Create referencer object
+    entity_referencer = EntityReferencer(sentence_filter.description_sentences)
+else:
+    # Create referencer object
+    entity_referencer = EntityReferencer(sentence_filter.logic_sentences)
+
+# Refer entities
+entity_referencer.refer_entities()
+
+# Replace logic sentences with references
+sample_sentences = " ".join(sentence for sentence in sentence_filter.logic_sentences)
+for word in entity_referencer.referencing_Dic:
+    sample_sentences = sample_sentences.replace(word, entity_referencer.referencing_Dic[word])
+
+
+
+# Start the process
+method_two()
--- com-logic/src/InputOutputIdentifier.py
+++ com-logic/src/InputOutputIdentifier.py
@@ -1,11 +1,12 @@
 from nltk import word_tokenize, sent_tokenize
 import re
 from nltk.stem import PorterStemmer
+from nltk.stem import WordNetLemmatizer
 
 
 class InputOutputIdentifier:
-    outputIndicators = ['then', 'will', 'activate''generate', 'create', 'give', 'turn', 'operate', 'start']
-    inputIndicators = ['and', 'if', 'only', 'when', 'either', 'all']
+    outputIndicators = ['then', 'will', 'activate''generate', 'create', 'give', 'turn', 'operate', 'start','unlock','to','ring','switch']
+    inputIndicators = ['and','or','if','only','when','either','all']
 
     def __init__(self, entity_set,text):
         self.entity_set = entity_set
@@ -15,28 +16,35 @@ class InputOutputIdentifier:
         self.neighbour_dic = {}
 
     def neighbourhood_based_identification(self):
-        ps = PorterStemmer()
-        sentences = sent_tokenize(self.text)
-        for sentence in sentences:
-            for entity in self.entity_set:
-                # get the surrounded words of a given word
-                r1 = re.search(r"(?:[a-zA-Z'-]+[^a-zA-Z'-]+){0,2}" + entity + "(?:[^a-zA-Z'-]+[a-zA-Z'-]+){0,2}",sentence)
+        # ps = PorterStemmer()
+        lemma = WordNetLemmatizer()
+        sentences = re.split(r'[,.]', self.text)
+        for entity in self.entity_set:
+            self.neighbour_dic[entity] = []
+            for sentence in sentences:
+                r1 = re.search(r"(?:[a-zA-Z'-]+[^a-zA-Z'-]+){0,3}" + entity + "(?:[^a-zA-Z'-]+[a-zA-Z'-]+){0,3}",sentence)
                 if r1 is None:
                     continue
                 else:
-                    print(r1.group())
-                    self.neighbour_dic[entity] = word_tokenize(r1.group());
+                    for i in word_tokenize(r1.group()):
+                        self.neighbour_dic[entity].append(i)
+                    # self.neighbour_dic[entity]= self.neighbour_dic[entity]+word_tokenize(r1.group())
 
         for entity in self.entity_set:
             neighbours = self.neighbour_dic[entity]
+            # print(neighbours)
+
             stem_neighbours = []
             for neighbour in neighbours:
-                stem_neighbours.append(ps.stem(neighbour))
+                stem_neighbours.append(lemma.lemmatize(neighbour, pos = 'v'))
             for stem_neighbour in stem_neighbours:
                 if stem_neighbour in InputOutputIdentifier.inputIndicators:  # check whether a stem word is inside input indicators
                     self.input_set.add(entity)
+                    break
+
                 elif stem_neighbour in InputOutputIdentifier.outputIndicators:  # check whether a stem word is inside output indicators
                     self.output_set.add(entity)
+                    break
 
 
 
